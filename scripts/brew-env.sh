854b11e3722aa376a3056c1e4ead249c3ba6e62a
Mike Gates <cmkrnl@gmail.com>
2021-01-01 08:50:55 -0700

    add jdk to brew
    Updated brew env script to include openjdk

--- scripts/brew-env.sh
+++ scripts/brew-env.sh
@@ -12,7 +12,7 @@ echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.zshrc
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install packages
-brew install wget gzip coreutils htop neofetch findutils moreutils
+brew install wget gzip coreutils htop neofetch findutils moreutils openjdk
 
 # install casks
 brew install --cask 4k-video-downloader atom google-chrome microsoft-teams 4k-youtube-to-m3 firefox handbrake openconnect-gui appcleaner google-backup-and-sync mattermost vlc iterm2 skype-for-business yammer transmission signal
