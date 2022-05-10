f8295d2da76fb31d40b59bfbaa399a88671d1f1a
Mike Gates <gates@hpe.com>
2022-05-10 11:40:24 -0600

    Update brew-env.sh
    added camo-studio

--- scripts/brew-env.sh
+++ scripts/brew-env.sh
@@ -20,7 +20,7 @@ brew install --cask 4k-video-downloader 4k-youtube-to-mp3	blockblock lulu transm
 brew install --cask adoptopenjdk discord-canary	mattermost oversight tuxera-ntfs
 brew install --cask appcleaner do-not-disturb	microsoft-auto-update	parallels	vlc
 brew install --cask atom firefox microsoft-teams reikey zoom spideroakone
-brew install --cask audacity knockknock	netiquette royal-tsx
+brew install --cask audacity knockknock	netiquette royal-tsx camo-studio
 
 #!
 exit 0
