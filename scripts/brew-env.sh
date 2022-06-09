21294e276bbfecbf30d980b882513926b11fdb47
Mike Gates <gates@hpe.com>
2022-06-08 21:55:01 -0600

    Update brew-env.sh
    remove atom, add vsc

--- scripts/brew-env.sh
+++ scripts/brew-env.sh
@@ -19,7 +19,7 @@ brew install --cask logitech-options google-chrome microsoft-office iterm2 mailt
 brew install --cask 4k-video-downloader 4k-youtube-to-mp3	blockblock lulu transmission
 brew install --cask adoptopenjdk discord mattermost oversight tuxera-ntfs malwarebytes
 brew install --cask appcleaner do-not-disturb	microsoft-auto-update	parallels	vlc
-brew install --cask atom firefox microsoft-teams reikey zoom spideroakone
+brew install --cask visual-studio-code firefox microsoft-teams reikey zoom spideroakone
 brew install --cask audacity knockknock	netiquette royal-tsx camo-studio
 
 #!
