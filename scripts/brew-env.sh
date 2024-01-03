e493af53ce56601c408125918957f39e14717812
Mike Gates <cmkrnl@gmail.com>
2024-01-03 09:56:23 -0700

    Update brew-env.sh
    remove camo-studio

--- scripts/brew-env.sh
+++ scripts/brew-env.sh
@@ -18,9 +18,9 @@ brew install wget gzip coreutils htop neofetch findutils moreutils openjdk smart
 brew install --cask logi-options-plus google-chrome microsoft-office iterm2 clickshare
 brew install --cask 4k-video-downloaderplus 4k-youtube-to-mp3 mailspring blockblock lulu
 brew install --cask adoptopenjdk discord oversight tuxera-ntfs malwarebytes transmission
-brew install --cask appcleaner do-not-disturb	microsoft-auto-update	parallels	vlc
+brew install --cask appcleaner do-not-disturb microsoft-auto-update	parallels vlc
 brew install --cask visual-studio-code firefox microsoft-teams reikey zoom opensc displaylink
-brew install --cask audacity knockknock	netiquette royal-tsx camo-studio ransomwhere
+brew install --cask audacity knockknock	netiquette royal-tsx ransomwhere
 
 #!
 exit 0
