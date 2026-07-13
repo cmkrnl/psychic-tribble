d94a1d14e6c8d71014e9577f84f7781449b15310
Mike Gates <cmkrnl@gmail.com>
2026-07-13 10:21:01 -0600

    update to env files
    adding docker & calibre to brew, update new files for latest Ubuntu distro

--- scripts/brew-env.sh
+++ scripts/brew-env.sh
@@ -15,7 +15,7 @@ echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.zshrc
 brew install wget gzip coreutils htop neofetch findutils moreutils openjdk smartmontools autoconf autoconf-archive automake ccache cmake nasm ninja pkg-config
 
 # install casks
-brew install --cask mac-mouse-fix google-chrome iterm2
+brew install --cask mac-mouse-fix google-chrome iterm2 docker-desktop calibre
 brew install --cask 4k-video-downloaderplus 4k-youtube-to-mp3 mailspring blockblock
 brew install --cask discord oversight transmission lulu temurin ungoogled-chromium
 brew install --cask appcleaner taskexplorer mullvad-vpn balenaetcher xdeck
